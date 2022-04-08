5848ec410d9ebea464a48da6f3bc3fe117ea6222
“SATHYA-RUNTIME” <sathyaforjob1@gmail.com>
2022-04-08 12:28:07 +0530

    fourth

--- ownqr/settings.py
+++ ownqr/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",os.path.join(BASE_DIR,'static'),]
+STATICFILES_DIRS = [BASE_DIR / "sta",os.path.join(BASE_DIR,'sta'),"/home/sathya/plan_B/ownqr"]
 STATIC_ROOT =os.path.join(BASE_DIR,'staticsfiles')
 
 # Default primary key field type
--- qrapp/views.py
+++ qrapp/views.py
@@ -10,10 +10,10 @@ def qr(request):
         qr=pyqrcode.create(content)
         ln=len(content)
         path1=str(ln)+".png"
-        #path=os.path.join("/home/sathya/plan_B/ownqr/static/img/",path1)
-        qr.png("path.png",scale=8)
+        #path=os.path.join("/home/sathya/plan_B/ownqr/sta/img/",path1)
+        qr.png(path1,scale=8)
         path2=ln
-        return render(request,"qr.html",{"fnme":path2})
+        return render(request,"qr.html",{"fnme":path2,"path":"path"})
     if request.method=="POST":    
         os.remove(path1)
     return render(request,"home.html")
